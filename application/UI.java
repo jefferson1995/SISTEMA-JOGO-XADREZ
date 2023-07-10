5a7280ff75ac696e61d4f4c9149507b219c4eeb0
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-07-10 08:33:33 -0300

    manipulação de peças capturadas

--- application/UI.java
+++ application/UI.java
@@ -5,8 +5,11 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 //Classe para imprimir tabuleiro
 public class UI {
@@ -60,8 +63,10 @@ public class UI {
     /*
         Imprimir a partida
      */
-    public static void printMatch(ChessMatch chessMatch){
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured){
         printBoard(chessMatch.getPieces());
+        System.out.println();
+        printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turno: " + chessMatch.getTurn());
         System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
@@ -108,4 +113,22 @@ public class UI {
         System.out.print(" ");
     }
 
+    /*Método responsável para imprimir as peças capturadas
+
+     */
+    private static void printCapturedPieces(List<ChessPiece> captured){
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        System.out.println("Peças capturadas: ");
+        System.out.print("Peças brancas: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray())); //Imprimir array de valores
+        System.out.print(ANSI_RESET);
+
+        System.out.print("Peças pretas: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray())); //Imprimir array de valores
+        System.out.print(ANSI_RESET);
+
+    }
 }
--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -11,12 +11,18 @@ import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
 
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -110,6 +116,10 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target); // Remove a possível peça que esteja na posição de destino.
         board.placePiece(p, target);
 
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -119,6 +129,7 @@ public class ChessMatch {
      */
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition()); //Converte para posição de xadrez
+        piecesOnTheBoard.add(piece);
     }
 
     /*
