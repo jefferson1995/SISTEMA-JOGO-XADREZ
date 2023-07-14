c399b43f0a374df7ed7f40dfba9e99f5130077bb
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-07-14 07:39:47 -0300

    Fix: Tratamento de exceção - promoted

--- application/Program.java
+++ application/Program.java
@@ -37,9 +37,16 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-                if(chessMatch.getPromoted() != null){
+                if (chessMatch.getPromoted() != null) {
                     System.out.print("Digite a peça promovida (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Digite a peça promovida novamente (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+
+
                     chessMatch.replacePromotedPiece(type);
                 }
 
--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -10,7 +10,6 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -137,7 +136,7 @@ public class ChessMatch {
             throw new IllegalStateException("Não existe peça para ser promovida");
         }
         if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("letra informada inválida para:  promotion ");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
